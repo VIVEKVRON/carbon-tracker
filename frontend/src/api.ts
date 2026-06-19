27c58696d058c9b6bd2da7b893b9119d2fea91a1
Vivek V Ron <vivekvron@gmail.com>
2026-06-20 00:32:19 +0530

    Update frontend API URL for production

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8080/api/v1';
+const BASE_URL = 'https://carbon-tracker-api-r1dx.onrender.com/';
 
 export const getAuthToken = () => localStorage.getItem('token');
 export const setAuthToken = (token: string) => localStorage.setItem('token', token);
